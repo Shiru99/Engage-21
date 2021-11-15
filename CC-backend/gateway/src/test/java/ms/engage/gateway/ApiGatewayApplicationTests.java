d7cb84ae492b262a1bcee45c148ba5b6be4bdd0e
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-15 11:51:59 +0530

    API - gateway added

---
+++ CC-backend/gateway/src/test/java/ms/engage/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.engage.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
