6429d0f40bf16d8b04e05b9c1b5d7a27e7dcd12c
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-17 10:42:48 +0530

    hystrix-dashboard Added

---
+++ CC-backend/hystrix-dashboard/src/test/java/ms/engage/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.engage.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
