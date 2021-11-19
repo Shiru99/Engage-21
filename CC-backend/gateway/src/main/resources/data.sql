8f30faaa81728f510b6f79eb10ab904a450992d4
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-19 16:28:42 +0530

    cc - users schema added

---
+++ CC-backend/gateway/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO
+    cc_users (id, username, password, active, roles)
+VALUES
+    (
+        1,
+        'JohnD',
+        '{bcrypt}$2a$10$jBxirFzCFwYVAskGcBMINuxjxJuD/gXIWqzkkCjtRhFpkzm9H1NPO',
+        'true',
+        'ROLE_USER'
+    ),
+    (
+        2,
+        'JennyD',
+        '{bcrypt}$2a$10$jBxirFzCFwYVAskGcBMINuxjxJuD/gXIWqzkkCjtRhFpkzm9H1NPO',
+        'true',
+        'ROLE_USER'
+    ),
+    (
+        3,
+        'JamesD',
+        '{bcrypt}$2a$10$jBxirFzCFwYVAskGcBMINuxjxJuD/gXIWqzkkCjtRhFpkzm9H1NPO',
+        'true',
+        'ROLE_USER'
+    ),
+;
+
+-- password : 1234
