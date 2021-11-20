4d00de64bdbc7eaf94f73bd4c7d25f5ffe2cca7c
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-20 11:05:35 +0530

    JWT Authentication conflict resolved

---
+++ CC-backend/gateway-II/target/classes/data.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+    cc_users (id, username, password, active, roles)
+VALUES
+    (
+        1,
+        'JohnD',
+        '{bcrypt}$2a$10$jBxirFzCFwYVAskGcBMINuxjxJuD/gXIWqzkkCjtRhFpkzm9H1NPO',
+        'true',
+        'ROLE_USER'
+    ),
+    (
+        2,
+        'JennyD',
+        '{bcrypt}$2a$10$jBxirFzCFwYVAskGcBMINuxjxJuD/gXIWqzkkCjtRhFpkzm9H1NPO',
+        'true',
+        'ROLE_USER'
+    ),
+    (
+        3,
+        'JamesD',
+        '{bcrypt}$2a$10$jBxirFzCFwYVAskGcBMINuxjxJuD/gXIWqzkkCjtRhFpkzm9H1NPO',
+        'true',
+        'ROLE_USER'
+    ),
+    (
+        4,
+        'temp',
+        '1234',
+        'true',
+        'ROLE_USER'
+    )
+;
+
+-- password : 1234
