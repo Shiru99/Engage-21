6adc37ddcfff37fdc16c4419e3d5aed0fd3f85e5
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-20 12:30:56 +0530

    few updates

--- CC-backend/gateway/src/main/java/ms/engage/gateway/Authentication/util/JwtUtil.java
+++ CC-backend/gateway/src/main/java/ms/engage/gateway/Authentication/util/JwtUtil.java
@@ -3,6 +3,8 @@ package ms.engage.gateway.Authentication.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,8 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String SECRET_KEY = "ms.engage.shiru99";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
