cdbc10cad9ee625b9b837550b81086e85485b205
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-23 17:13:54 +0530

    Authentication micro-service added

--- CC-backend/feature microservices/my-authentication-service/src/main/java/ms/engage/myauthenticationservice/Repository/CCUserRepository.java
+++ CC-backend/feature microservices/my-authentication-service/src/main/java/ms/engage/myauthenticationservice/Repository/CCUserRepository.java
@@ -2,12 +2,12 @@ package ms.engage.myauthenticationservice.Repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ms.engage.myauthenticationservice.Model.CCUser;
 
 @Repository
-public interface CCUserRepository extends JpaRepository<CCUser,Integer> {
+public interface CCUserRepository extends CrudRepository<CCUser,Integer> {
     Optional<CCUser> findByUsername(String username);
 }
