cb85c32ef057e2b16aa365b57a8fd19d82121a52
Shiru99 <shriram.ghadge1999@gmail.com>
2021-11-26 14:28:43 +0530

    Added JWT authentication with API-Gateway

---
+++ CC-backend/gateway (NOT Working)/src/main/java/ms/engage/gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package ms.engage.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "ms.engage")
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class ApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
